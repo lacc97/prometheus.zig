ac4e69efdb81d5f6586fe8d74e7f3bdc2bc29bc1
Luis Cáceres <lacc97@protonmail.ch>
2024-03-21 20:18:35 +0000

    Add more metrics to example, including floats and gauges

--- examples/full/src/main.zig
+++ examples/full/src/main.zig
@@ -21,11 +21,18 @@ pub fn main() !void {
     var tcp_rx_counter: prometheus.Counter(.int) = .{};
     try family.add(&tcp_rx_counter, .{ .protocol = .tcp, .direction = .rx });
 
+    var tcp_tx_counter: prometheus.Counter(.int) = .{};
+    try family.add(&tcp_tx_counter, .{ .protocol = .tcp, .direction = .tx });
+
     const family_2 = try registry.family("my_float", "Floaty number of float thingies", .{
         .metric_type = .float_gauge,
         .Dimensions = struct { opt_label: ?enum { a, b } = null, name: []const u8, id: u16 },
     });
 
-    var floaty: prometheus.Gauge(.float) = .{};
-    try family_2.add(&floaty, .{ .name = "hello", .id = 0 });
+    const floaties = try gpa.alloc(prometheus.Gauge(.float), 16);
+    defer gpa.free(floaties);
+    for (floaties, 0..) |*f, i| {
+        f.* = .{};
+        try family_2.add(f, .{ .name = "hello", .id = @intCast(i) });
+    }
 }
